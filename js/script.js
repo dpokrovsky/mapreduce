44e8ff88392a9fa292dc6738814bb91c2b2f707b
Dmitry Pokrovsky <dpokrovsky@hotmail.com>
2023-07-16 23:38:06 +0300

    v1.5

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 document.addEventListener("DOMContentLoaded", event => {
   document.getElementById("file").addEventListener("change", event => {
-    var file = event.target.files[0];
-    
+    const file = event.target.files[0];
+
     read(file)
       .then(content => { 
         document.getElementById("content").value = content;
-        
-        var maps = content
-        .replace(/[^-0-9a-zA-Z\u0410-\u042F\u0430-\u044F\s]/g, "")
-          .split(/\s+/)
-            .reduce((map, word) => {
-              map[word] = (map[word]||0)+1;
-              return map;
-            }, Object.create(null));
-  
-        content_reduced = 
-          JSON.stringify(maps)
-            .replace(/[^-0-9a-zA-Z\u0410-\u042F\u0430-\u044F\s\d:,]/g, "")
-              .replace(/,/g, "\n")
-                .replace(/:/g, " ");
+
+        const maps = content
+          .replace(/[^-0-9a-zA-Z\u0410-\u042F\u0430-\u044F\s]/g, "")
+            .split(/\s+/)
+              .reduce((map, word) => {
+                if (entry = map.find(entry => entry.key == word)) {
+                  entry.value ++;
+                  return map;
+                }
+                map.push({key: word, value: 1});
+                return map;
+              }, [])
+                .sort((a, b) => {
+                  if (a.key < b.key) return -1;
+                  if (a.key > b.key) return 1;
+                  return 0;
+                });
+
+        let content_reduced = "";
+
+        for (const map of maps) content_reduced += map.key + " " + map.value + "\n"; 
+
         document.getElementById("content_reduced").value = content_reduced;
 
         download(content_reduced, file.name);
@@ -28,7 +36,6 @@ document.addEventListener("DOMContentLoaded", event => {
 
   function read(file) {
     const reader = new FileReader();
-    
     return new Promise((resolve, reject) => {
       reader.onload = event => resolve(event.target.result);
       reader.onerror = error => reject(error);
@@ -40,9 +47,11 @@ document.addEventListener("DOMContentLoaded", event => {
     const download = document.getElementById("download");
     
     download.innerText = filename.replace(".txt", "_reduced.txt");
+
     download.style.display = "block";
+
     download.onclick = () => {
-      var element = document.createElement('a');
+      const element = document.createElement('a');
       element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content_reduced));
       element.setAttribute('download', download.innerText);
       element.style.display = 'none';
